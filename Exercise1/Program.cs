ab8e37b1db78e8b2457f23f5d23a9b34267c2af6
kidus Girma M <kidusgirma20@gmail.com>
2022-07-02 23:47:30 +0300

    Exercise 1 for LAB Class

---
+++ Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise1
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Please Enter a Value: ");
+            int num = Convert.ToInt32(Console.ReadLine()) ;
+            if(num % 3 == 0 & num % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+                Console.WriteLine(num);
+
+
+        }
+    }
+}
