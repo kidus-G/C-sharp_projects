047b0abc2c8a7426bf44866a2862d95616922eef
kidus-github <kidusgirma20@gmail.com>
2022-08-02 13:18:30 +0300

    windows form exercise

---
+++ Exercise-3 & 4/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace Tutorial
+{
+    public partial class Form1 : Form
+    {
+        product pro = new product();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (idnum.Text == "")
+                MessageBox.Show("id number can't be empty");
+            else if (string.IsNullOrEmpty(objName.Text))
+                errorProvider1.SetError(objName, "Name is Required");
+            else
+            {
+                errorProvider1.Clear();
+                pro.ItemName = objName.Text;
+                pro.inventorynumber =invnum.Text;
+                pro.id = idnum.Text;
+                pro.date = dpicker.Value.ToString();
+                pro.count = Int32.Parse(cnt.Text);
+                pro.save();
+                pro.showAll();
+            }
+        }
+    }
+}
---
+++ Exercise-3 & 4/product.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp1
+{
+    public class product
+    {
+        public string inventorynumber { get; set; }
+        public string id { get; set; }
+        public string price { get; set; }
+        public string ItemName { get; set; }
+        public int count { get; set; }
+        public string date { get; set; }
+
+       static List<product> products = new List<product>();
+
+        
+
+        public void save()
+        {
+            products.Add(this);
+        }
+        public void showAll()
+        {
+            for(int i = 0; i < products.Count; i++)
+            {
+            }
+        }
+
+    }
+}
